56d90d42cc16d4514988cab447133984176ca8d7
Sidsai <82812380+Sidsai@users.noreply.github.com>
2022-05-29 00:15:05 +0530

    Add files via upload

---
+++ WEB-INF/src/MyConnection.java
@@ -0,0 +1,18 @@
+import java.sql.*;
+import javax.swing.JOptionPane;
+ public class MyConnection {
+    public Connection connectme()
+    {
+       try
+       {
+           Class.forName("oracle.jdbc.driver.OracleDriver");
+           return DriverManager.getConnection("jdbc:oracle:thin:@DESKTOP-E01S90C:1521:XE","siddharth","siddharth");
+           //return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","dsouza","dsouza");
+       }
+       catch(ClassNotFoundException | SQLException e)
+       {
+           JOptionPane.showMessageDialog(null,e);
+       }
+       return null;
+    }
+}
